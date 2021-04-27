c26338e1dca5a5976572377a380e50bc22da921a
Tehuel <torresbaldi.tehuel@gmail.com>
2021-04-27 20:51:26 -0300

    adds turret sound

--- clase-3/entities/Projectile.gd
+++ clase-3/entities/Projectile.gd
@@ -4,7 +4,6 @@ onready var timer = $DeleteTimer
 var speed = 250.0
 var direction
 
-
 func initialize(initial_direction, initial_position):
 	global_position = initial_position
 	direction = initial_direction
@@ -12,7 +11,6 @@ func initialize(initial_direction, initial_position):
 	
 func _physics_process(delta):
 	position += direction * speed * delta
-
 
 func _on_DeleteTimer_timeout():
 	get_parent().remove_child(self)
--- clase-3/entities/turret/turret.gd
+++ clase-3/entities/turret/turret.gd
@@ -1,13 +1,23 @@
 extends Sprite
 
 onready var cannon = $cannon
+onready var timer = $ShootTimer
+onready var attack_sound = $AttackSound
 var target
 
+var rng = RandomNumberGenerator.new()
+
 func initialize(projectile_container, player):
+	cannon.container = projectile_container
 	target = player
-	pass
-#	cannon.container = projectile_container
+	rng.randomize()
+	timer.wait_time = rng.randf_range(2, 5)
+	timer.start()
 
 func _physics_process(delta):
 	# Movimiento cañon	
 	cannon.rotation = (target.global_position - global_position).angle()
+
+func _on_ShootTimer_timeout():
+	attack_sound.play()
+	cannon.fire()
